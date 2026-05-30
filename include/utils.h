accd37f8e839ce0adbc93ce6d19441404b036e26
githaen <nantadryanek4@gmail.com>
2026-05-31 03:49:20 +0700

    fix conditional jump or move depends on uninitialized valu

--- include/utils.h
+++ include/utils.h
@@ -7,10 +7,15 @@ struct audio_song {
     struct audio_song* next;
     struct audio_song* prev;
 
+    int idList;
     std::string songTitle;
     std::string filepath;
 
-    audio_song(std::string s, std::string f) : songTitle(s), filepath(f) {}
+    audio_song(int idList, std::string s, std::string f)
+        : idList(idList), songTitle(s), filepath(f) {
+        prev = nullptr;
+        next = nullptr;
+    }
 };
 
 void data_callback(ma_device* pDevice, void* pOutput, const void* pInput,
